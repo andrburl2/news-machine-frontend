c5eb538e78427479c20dae257d65abb1178808ca
Andrey Burlakov <andrburl2@yandex.ru>
2020-04-30 11:32:07 +0300

    Настроил инфраструктуру git и webpack

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+    secondary: './src/secondary/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]/[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: { loader: "babel-loader" },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: './images/[name].[ext]',
+              esModule: false
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]&publicPath=../',
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name]/[name].[contenthash].css' }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/secondary/index.html',
+      filename: './secondary/index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+}
